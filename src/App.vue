c6c9a0643a3b823a9b6f6e4aad8e14abe0ebebd7
viet34tqc <viet34tqc@gmail.com>
2022-10-27 15:36:27 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterView } from "vue-router";
+import SiteNavigation from "./components/SiteNavigation.vue";
+</script>
+<template>
+  <div class="flex flex-col min-h-screen bg-weather-primary">
+    <SiteNavigation />
+    <RouterView />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
